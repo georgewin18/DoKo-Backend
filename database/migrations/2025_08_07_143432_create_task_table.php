6648e685070fb3bb2d8a57803cf13e00f77afcf8
georgewin18 <georgewk1809@gmail.com>
2025-08-07 22:53:18 +0700

    feat(db-migration): task table migration

---
+++ database/migrations/2025_08_07_143432_create_task_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->text('attachment')->nullable();
+            $table->date('date');
+            $table->time('time');
+            $table->integer('progress')->nullable();
+
+            $table->foreignId('task_group_id')
+                ->constrained('task_group')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('task');
+    }
+};
