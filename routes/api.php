f6d535e205224f2a030695c13dacbb4bc870b68b
georgewin18 <georgewk1809@gmail.com>
2025-10-28 16:01:09 +0700

    feat(routes): add focus timer to API routes

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,11 @@
 
 use App\Http\Controllers\TaskGroupController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\FocusTimerController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('task-groups', TaskGroupController::class);
 
 Route::resource('tasks', TaskController::class);
+
+Route::resource('focus-timer', FocusTimerController::class);
